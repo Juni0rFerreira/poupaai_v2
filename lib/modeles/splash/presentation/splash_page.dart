8ec707dfc80bdbc6eca98d2f758ca88dd863e009
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2023-11-11 02:20:56 -0300

    commit

---
+++ lib/modeles/splash/presentation/splash_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFF00A9FF), Color(0xFF89CFF3)]),
+        ),
+        child: const Text('PoupaAí'),
+      ),
+    );
+  }
+}
